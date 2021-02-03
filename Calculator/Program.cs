d55ffc1c1bd78c3f6a4176559ab210303267b101
Iosif Vasou <Iosifvasoul@yahoo.gr>
2021-02-03 15:41:46 +0200

    First commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Choise;
+            int Number1;
+            int Number2;
+            int Result;
+
+            Console.WriteLine("Please select an option from the list: ");
+            Console.WriteLine("1:  Add");
+            Console.WriteLine("2:  Substruct");
+            Console.WriteLine("3:  Multiply");
+            Console.WriteLine("4:  Divide");
+            Choise = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please select the first number: ");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please select the second number: ");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (Choise)
+            {
+                case 1:
+                    Result = Number1 + Number2;
+                    Console.WriteLine("Your result: " + Result);
+                    break;
+                case 2:
+                    Result = Number1 - Number2;
+                    Console.WriteLine("Your result: " + Result);
+                    break;
+                case 3:
+                    Result = Number1 * Number2;
+                    Console.WriteLine("Your result: " + Result);
+                    break;
+                case 4:
+                    Result = Number1 / Number2;
+                    Console.WriteLine("Your result: " + Result);
+                    break;
+
+            }
+        }
+    }
+}
